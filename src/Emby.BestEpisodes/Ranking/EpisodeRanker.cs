c8d6951926fc87ac440174097388e820afd3018e
Conroy <dannyconroy@hotmail.com>
2026-08-04 20:05:25 +0100

    Add src/Emby.BestEpisodes/Ranking/EpisodeRanker.cs

---
+++ src/Emby.BestEpisodes/Ranking/EpisodeRanker.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Emby.BestEpisodes.Ranking
+{
+    public static class EpisodeRanker
+    {
+        public static IReadOnlyDictionary<int, IReadOnlyList<EpisodeCandidate>> RankBySeason(
+            IEnumerable<EpisodeCandidate> episodes,
+            int maximumPerSeason,
+            double minimumRating,
+            bool includeUnrated,
+            bool includeSpecials)
+        {
+            var limit = maximumPerSeason < 1 ? 1 : maximumPerSeason;
+            var floor = minimumRating < 0 ? 0 : minimumRating > 10 ? 10 : minimumRating;
+
+            return episodes
+                .Where(item => includeSpecials || item.SeasonNumber > 0)
+                .Where(item => includeUnrated || item.CommunityRating.HasValue)
+                .Where(item => !item.CommunityRating.HasValue || item.CommunityRating.Value >= floor)
+                .GroupBy(item => item.SeasonNumber)
+                .OrderBy(group => group.Key)
+                .ToDictionary(
+                    group => group.Key,
+                    group => (IReadOnlyList<EpisodeCandidate>)group
+                        .OrderByDescending(item => item.CommunityRating.HasValue)
+                        .ThenByDescending(item => item.CommunityRating ?? float.MinValue)
+                        .ThenBy(item => item.EpisodeNumber ?? int.MaxValue)
+                        .ThenBy(item => item.Name)
+                        .Take(limit)
+                        .ToList());
+        }
+    }
+}
+
