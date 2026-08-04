da632fe2d6aaf8b5e7d1ce27d6d35118f3120b57
Conroy <dannyconroy@hotmail.com>
2026-08-04 22:25:59 +0100

    Update src/Emby.BestEpisodes/Plugin.cs for v0.2.0

--- src/Emby.BestEpisodes/Plugin.cs
+++ src/Emby.BestEpisodes/Plugin.cs
@@ -1,6 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Emby.Web.GenericEdit.Common;
 using MediaBrowser.Common;
 using MediaBrowser.Common.Plugins;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Entities.TV;
+using MediaBrowser.Controller.Library;
 using MediaBrowser.Controller.Plugins;
 using MediaBrowser.Model.Logging;
 
@@ -9,11 +16,16 @@ namespace Emby.BestEpisodes
     public sealed class Plugin : BasePluginSimpleUI<PluginOptions>
     {
         private static readonly Guid PluginId = new Guid("15C9C024-9067-4AA1-B94A-A9FF35B7A767");
+        private readonly ILibraryManager _libraryManager;
         private readonly ILogger _logger;
 
-        public Plugin(IApplicationHost applicationHost, ILogManager logManager)
+        public Plugin(
+            IApplicationHost applicationHost,
+            ILogManager logManager,
+            ILibraryManager libraryManager)
             : base(applicationHost)
         {
+            _libraryManager = libraryManager;
             _logger = logManager.GetLogger(Name);
             Options = GetOptions();
         }
@@ -25,14 +37,50 @@ namespace Emby.BestEpisodes
         public override string Name => PluginName;
 
         public override string Description =>
-            "Creates a highest-rated episode playlist for each season of a configured TV series.";
+            "Creates highest-rated episode playlists for each season of selected TV series.";
 
         public override Guid Id => PluginId;
+
+        protected override PluginOptions OnBeforeShowUI(PluginOptions options)
+        {
+            var current = options ?? new PluginOptions();
+            current.SeriesOptions = GetSeriesOptions();
+            return current;
+        }
 
         protected override void OnOptionsSaved(PluginOptions options)
         {
             Options = options ?? new PluginOptions();
             _logger.Info("{0} options saved. Run the scheduled task to refresh playlists.", Name);
+        }
+
+        private IReadOnlyList<EditorSelectOption> GetSeriesOptions()
+        {
+            var series = _libraryManager.GetItemList(new InternalItemsQuery
+                {
+                    IncludeItemTypes = new[] { "Series" },
+                    Recursive = true
+                })
+                .OfType<Series>()
+                .OrderBy(item => item.Name, StringComparer.OrdinalIgnoreCase)
+                .ThenBy(item => item.InternalId)
+                .ToList();
+
+            var duplicateNames = new HashSet<string>(
+                series.GroupBy(item => item.Name ?? string.Empty, StringComparer.OrdinalIgnoreCase)
+                    .Where(group => group.Count() > 1)
+                    .Select(group => group.Key),
+                StringComparer.OrdinalIgnoreCase);
+
+            return series.Select(item => new EditorSelectOption
+                {
+                    Value = item.InternalId.ToString(CultureInfo.InvariantCulture),
+                    Name = duplicateNames.Contains(item.Name ?? string.Empty)
+                        ? string.Format("{0} (library ID {1})", item.Name, item.InternalId)
+                        : item.Name,
+                    IsEnabled = true
+                })
+                .ToList();
         }
     }
 }
